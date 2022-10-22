18ca672b02619e45d0678a21f6952bca17ae1539
Ulan <ulalaseit@gmail.com>
2022-10-22 14:42:42 +0600

    Initial Commit

---
+++ InternTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternTestTask
+//
+//  Created by Ulan Seitkali on 22.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
