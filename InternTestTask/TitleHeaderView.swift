177459decbd1ec05423febc6546abab831bf5725
Ulan <ulalaseit@gmail.com>
2022-10-25 01:18:44 +0600

    Make Header View

---
+++ InternTestTask/TitleHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleHeaderView.swift
+//  InternTestTask
+//
+//  Created by Ulan Seitkali on 24.10.2022.
+//
+
+import UIKit
+
+class TitleHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
